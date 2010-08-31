6e8aed76bfd97b2ae274e34a6a3533dabcc02b99
Ullrich Schäfer <ullrich@seidbereit.de>
2010-08-31 18:38:01 +0200

    Fixed deprecation warning on IOS

--- Sources/OAuth2Client/NXOAuth2PostBodyPart.m
+++ Sources/OAuth2Client/NXOAuth2PostBodyPart.m
@@ -71,7 +71,7 @@
     [headers appendString:@"Content-Type: application/octet-stream\r\n"];
 	[headers appendString:@"\r\n"];
 	
-#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6
+#if MAC_OS_X_VERSION_MIN_REQUIRED >= MAC_OS_X_VERSION_10_6 || TARGET_OS_IPHONE
 	NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:path error:nil];
 #else
     NSDictionary *fileAttributes = [[NSFileManager defaultManager] fileAttributesAtPath:path traverseLink:YES];
