aa97d01e4730d8e6d40255344b3e44881e6d1566
Tobias Kräntzer <info@tobias-kraentzer.de>
2011-12-21 14:09:04 +0100

    Fix bug in writing the content size of the HTTP request.

--- Sources/OAuth2Client/NXOAuth2Connection.m
+++ Sources/OAuth2Client/NXOAuth2Connection.m
@@ -237,7 +237,7 @@ sendingProgressHandler:(NXOAuth2ConnectionSendingProgressHandler)aSendingProgres
 		NSInputStream *postBodyStream = [[NXOAuth2PostBodyStream alloc] initWithParameters:parameters];
 		
 		NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@", [(NXOAuth2PostBodyStream *)postBodyStream boundary]];
-		NSString *contentLength = [NSString stringWithFormat:@"%d", [(NXOAuth2PostBodyStream *)postBodyStream length]];
+		NSString *contentLength = [NSString stringWithFormat:@"%lld", [(NXOAuth2PostBodyStream *)postBodyStream length]];
 		[aRequest setValue:contentType forHTTPHeaderField:@"Content-Type"];
 		[aRequest setValue:contentLength forHTTPHeaderField:@"Content-Length"];
 		
