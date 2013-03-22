a7583c9da5256378d5329a840fb7c9f03fd7c6fb
Marc Ransome <marc.ransome@fidgetbox.co.uk>
2013-03-22 09:48:45 +0000

    Adding explicit 'Accept' request-header field for application/json media type

--- Sources/OAuth2Client/NXOAuth2Connection.m
+++ Sources/OAuth2Client/NXOAuth2Connection.m
@@ -197,6 +197,8 @@ sendingProgressHandler:(NXOAuth2ConnectionSendingProgressHandler)aSendingProgres
         [startRequest setValue:client.userAgent forHTTPHeaderField:@"User-Agent"];
     }
     
+    [startRequest setValue:@"application/json" forHTTPHeaderField:@"Accept"];
+    
     NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:startRequest delegate:self startImmediately:NO];    // don't start yet
     [aConnection scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];    // let's first schedule it in the current runloop. (see http://github.com/soundcloud/cocoa-api-wrapper/issues#issue/2 )
     [aConnection start];    // now start
