ed1abeee1f7a352d9bfe83ba1302cf9b25bffcf4
Przemek Szalko <p.szalko@gmail.com>
2014-01-07 15:26:16 +0100

    Fixed assertion issue when refresh_token is not used and access_token is expired.

--- Sources/OAuth2Client/NXOAuth2Connection.m
+++ Sources/OAuth2Client/NXOAuth2Connection.m
@@ -169,7 +169,7 @@ sendingProgressHandler:(NXOAuth2ConnectionSendingProgressHandler)aSendingProgres
         // if token is expired don't bother starting this connection.
         NSDate *tenSecondsAgo = [NSDate dateWithTimeIntervalSinceNow:(-10)];
         NSDate *tokenExpiresAt = client.accessToken.expiresAt;
-        if ([tenSecondsAgo earlierDate:tokenExpiresAt] == tokenExpiresAt) {
+        if (nil != client.accessToken.refreshToken && [tenSecondsAgo earlierDate:tokenExpiresAt] == tokenExpiresAt) {
             [self cancel];
             [client refreshAccessTokenAndRetryConnection:self];
             return nil;
