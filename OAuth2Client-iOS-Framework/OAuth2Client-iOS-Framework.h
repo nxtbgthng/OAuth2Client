b42ca0b893469bfd55625f9d885f2003e8bf2904
Alexander Dodatko <dodikk88.reg@gmail.com>
2015-02-08 10:57:47 +0200

    Added iOS dynamic framework target

---
+++ OAuth2Client-iOS-Framework/OAuth2Client-iOS-Framework.h
@@ -0,0 +1,19 @@
+//
+//  OAuth2Client-iOS-Framework.h
+//  OAuth2Client-iOS-Framework
+//
+//  Created by dodikk on 2/8/15.
+//  Copyright (c) 2015 nxtbgthng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OAuth2Client-iOS-Framework.
+FOUNDATION_EXPORT double OAuth2Client_iOS_FrameworkVersionNumber;
+
+//! Project version string for OAuth2Client-iOS-Framework.
+FOUNDATION_EXPORT const unsigned char OAuth2Client_iOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OAuth2Client_iOS_Framework/PublicHeader.h>
+
+
